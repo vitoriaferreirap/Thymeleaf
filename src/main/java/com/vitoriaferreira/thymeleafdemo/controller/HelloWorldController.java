356321ac25c19188def4078ca10a34866f0ba01b
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-06-28 21:12:27 -0300

    feacture: usando @RequestParams para leitura de dados de formulário

--- src/main/java/com/vitoriaferreira/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/vitoriaferreira/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.vitoriaferreira.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -12,8 +16,38 @@ public class HelloWorldController {
     }
 
     // novo metodo controlle para processar um forms html
+
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld"; // retorna o nome do template HTML
+    }
+
+    // novo metodo controlle para ler um forms data e add data no model
+    @RequestMapping("/processFormVerisionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // ler o request data do form HTML
+        String theName = request.getParameter("studentName");
+        // converte para maiusculo
+        theName = theName.toUpperCase();
+        // criar uma mensagem
+        String result = "Yo! " + theName;
+        // add mensagem ao model
+        model.addAttribute("message", result);
+
+        return "helloworld"; // retorna o nome do template HTML
+    }
+
+    @RequestMapping("/processFormVerisionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // converte para maiusculo
+        theName = theName.toUpperCase();
+        // criar uma mensagem
+        String result = "Hey My Friend from v3! " + theName;
+        // add mensagem ao model
+        model.addAttribute("message", result);
+
         return "helloworld"; // retorna o nome do template HTML
     }
 }
